9278c573378e8c49b86b1258e81e72c85c43b5a9
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-03-05 12:15:31 +0100

    added cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const colors = require('colors');
 
 const app = express();
+
+app.use(cors());
 
 app.listen(3000, () => console.log("Server is upp...".rainbow.bold.italic));
