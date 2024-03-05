c681f67cb5ad1bcb6345cc0f3920b59945c7e146
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-03-05 12:01:47 +0100

    color installed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const colors = require('colors');
+
 const app = express();
 
-app.listen(3000, () => console.log("Server is upp..."));
+app.listen(3000, () => console.log("Server is upp...".rainbow.bold.italic));
