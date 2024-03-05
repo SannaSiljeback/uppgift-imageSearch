1b37497cd2390976600ea220c5ed1e49123d29ec
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-03-05 15:07:13 +0100

    added axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,9 @@ import { LoginButton } from "./components/LoginButton";
 import { LogoutButton } from "./components/LogoutButton";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
 
-  const {isAuthenticated} = useAuth0();
-
-  return (
-    <>
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
-    </>
-  );
+  return <>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</>;
 }
 
 export default App;
