25569d18a53d860faeaa4ecf8c6228b1a30671fe
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-03-06 10:59:19 +0100

    created sreachForm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,22 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "./App.css";
 import { LoginButton } from "./components/LoginButton";
 import { LogoutButton } from "./components/LogoutButton";
+import { SearchForm } from "./components/SearchForm";
 
 function App() {
   const { isAuthenticated } = useAuth0();
 
-  return <>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</>;
+  return (
+    <>
+      {isAuthenticated ? (
+        <>
+          <SearchForm /> <LogoutButton />
+        </>
+      ) : (
+        <LoginButton />
+      )}
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/SearchForm.tsx
@@ -0,0 +1,8 @@
+export const SearchForm = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Vad vill du söka på?" />
+      <button type="submit">Sök</button>
+    </form>
+  );
+};
