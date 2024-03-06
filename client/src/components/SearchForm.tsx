7fa5a15e53df9d3e5f1e0043b9f4c69b90faa426
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-03-06 12:17:25 +0100

    made some functions

--- client/src/components/SearchForm.tsx
+++ client/src/components/SearchForm.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export const SearchForm = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+    const handleSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
+        //anropar handleSearch och url 
+    }
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+
+
   return (
-    <form>
-      <input type="text" placeholder="Vad vill du söka på?" />
-      <button type="submit">Sök</button>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Sök efter bilder!" value={inputValue} onChange={handleInput} />
+      <button type="submit" onClick={handleSearch}>Sök</button>
     </form>
   );
 };
