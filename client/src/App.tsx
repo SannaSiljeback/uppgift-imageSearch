a7f093ff1fe0810ded91dd650ffaffb62269c263
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-03-08 13:45:50 +0100

    tried a post on server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   const { isAuthenticated } = useAuth0();
 
   const saveUser = async () => {
+    //startas igång när man trycker på en knapp för favvo bilder
     try {
       const response = await axios.post(
         "http://localhost:3001/api/users",
@@ -20,7 +21,7 @@ function App() {
           headers: {
             "Content-Type": "application/json",
           },
-          //en body stringify här?
+          
         }
       );
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 
 const colors = require('colors');
 
@@ -10,7 +11,29 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/users", (req, res) => {
-    console.log(req.body);
+    console.log("user data", req.body);
+
+    fs.readFile("users.json", "utf8", (err, data) => {
+        if (err) {
+            console.log("kunde ej läsa filen", err);
+            return res.status(500).send("kunde ej läsa filen 2");
+        }
+
+        const users = JSON.parse(data);
+
+        users.push(req.body);
+
+        fs.writeFile("users.json", JSON.stringify(users), (err) => {
+            if (err) {
+                console.log("kunde ej skriva till filen", err);
+                return res.status(500).send("kunde ej skriva till filen 2");
+            }
+
+            res.status(201).send("användare är sparad");
+        });
+
+    });
+
 });
 
 app.listen(3000, () => console.log("Server is upp...".rainbow.bold.italic));
