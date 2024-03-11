da4c148bf1d7cf24ca12a0206f271f5062202edd
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-03-11 10:30:46 +0100

    changed place for saveUser

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,28 +8,6 @@ import axios from "axios";
 function App() {
   const { isAuthenticated } = useAuth0();
 
-  const saveUser = async () => {
-    //startas igång när man trycker på en knapp för favvo bilder?
-    try {
-      const response = await axios.post(
-        "http://localhost:3000/api/users",
-        {
-          userName: "testUser",
-          favoriteImage: "testImage",
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      console.log("användaren är sparad", response.data);
-    } catch (error) {
-      console.log("användaren sparades inte", error);
-    }
-  };
-
   return (
     <>
       {isAuthenticated ? (
--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -1,15 +1,41 @@
 import React from "react";
 import { ISearchResults } from "../models/ISearchResults";
+import axios from "axios";
 
 export const SearchResults: React.FC<{ results: ISearchResults[] }> = ({
   results,
 }) => {
+
+  const saveUser = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/users",
+        //ska /api vara med innan users?
+        {
+          userName: "testUser",
+          favoriteImage: "testImage",
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("användaren är sparad", response.data);
+    } catch (error) {
+      console.log("användaren sparades inte", error);
+    }
+  };
+
+
   return (
     <>
       {results.map((result, index) => (
         <div key={index}>
           <h2>{result.title}</h2>
           <img src={result.link} alt={result.title} />
+          <button onClick={saveUser}>Favvo knappen</button>
         </div>
       ))}
     </>
